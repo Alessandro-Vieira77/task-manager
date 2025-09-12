03dd2c60ba6a39555122a27d98a3a8904551b556
Alessandro-Vieira77 <alessandrovieiraa77@gmail.com>
2025-09-12 17:53:47 -0300

    chore: add eslint-plugin-simple-import-sort

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,16 @@
 import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
 import { defineConfig, globalIgnores } from "eslint/config";
 import prettier from "eslint-plugin-prettier";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import globals from "globals";
 
 export default defineConfig([
   globalIgnores(["dist"]),
   {
     files: ["**/*.{js,jsx}"],
-    plugins: { prettier },
+    plugins: { prettier, "simple-import-sort": simpleImportSort },
     extends: [
       js.configs.recommended,
       reactHooks.configs["recommended-latest"],
@@ -26,6 +27,8 @@ export default defineConfig([
     },
     rules: {
       "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
     },
   },
 ]);
