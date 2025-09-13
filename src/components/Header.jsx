28f5aff9dcc05a4f127b1ec089ae403afc67ee66
Alessandro-Vieira77 <alessandrovieiraa77@gmail.com>
2025-09-12 23:08:06 -0300

    feat: added responsiveness to the Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { IoMdAdd } from "react-icons/io";
 import Button from "./Button";
 function Header() {
   return (
-    <div className="flex w-full justify-between px-8 pt-16">
+    <div className="flex w-full flex-col justify-between gap-3 px-8 pt-16 lg:flex-row">
       <div className="flex flex-col gap-1">
         <h3 className="text-xs font-semibold text-[#00ADB5]">Minhas Tarefas</h3>
         <h2 className="text-xl font-semibold text-[#35383E]">Minhas Tarefas</h2>
