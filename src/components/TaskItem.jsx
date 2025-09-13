5201804331d10378501a6e3c7342cc8998d04be8
Alessandro-Vieira77 <alessandrovieiraa77@gmail.com>
2025-09-13 13:38:25 -0300

    feat: added variants to TaskItem and Input box

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,16 +1,48 @@
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import { MdOutlineDone } from "react-icons/md";
+import { tv } from "tailwind-variants";
+
+const taskItem = tv({
+  base: "w-full rounded-lg p-3 text-sm",
+  variants: {
+    status: {
+      completed: "bg-[#00ADB5]/10 text-[#00ADB5]",
+      pending: "bg-[#FFAA04]/10 text-[#FFAA04]",
+      notStaged: "bg-[#818181]/10 text-[#818181]",
+    },
+  },
+  defaultVariants: {
+    status: "notStaged",
+  },
+});
+
+const box = tv({
+  base: "relative flex h-7 w-7 cursor-pointer items-center justify-center rounded-lg",
+  variants: {
+    status: {
+      completed: "bg-[#00ADB5]",
+      pending: "bg-[#FFAA04] ",
+      notStaged: "bg-[#818181]",
+    },
+  },
+  defaultVariants: {
+    status: "notStaged",
+  },
+});
+
 function TaskItem() {
   return (
-    <div className="w-full rounded-lg bg-[#818181]/20 p-3 text-sm">
+    <div className={taskItem({ status: "completed" })}>
       <div className="flex items-center gap-2">
-        <label className="relative flex h-7 w-7 cursor-pointer items-center justify-center rounded-lg bg-[#818181]">
+        <label className={box({ status: "completed" })}>
           <input
             type="checkbox"
             checked={true}
             className="absolute h-full w-full cursor-pointer opacity-0"
           />
-          <span className="text-2xl"></span>
+          <span></span>
         </label>
-        <p className="text-sm font-medium text-[#818181]">aprendendo React</p>
+        <p className="text-sm font-medium">aprendendo React</p>
       </div>
     </div>
   );
