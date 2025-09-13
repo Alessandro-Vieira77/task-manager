411d4e8a1771c8da06e5a2886e09546bca346624
Alessandro-Vieira77 <alessandrovieiraa77@gmail.com>
2025-09-12 22:59:46 -0300

    feat: add ContainerTask

---
+++ src/components/ContainerTask.jsx
@@ -0,0 +1,7 @@
+function ContainerTask({ children }) {
+  return (
+    <div className="flex w-full flex-col gap-6 rounded-lg bg-white p-6 shadow">{children}</div>
+  );
+}
+
+export default ContainerTask;
