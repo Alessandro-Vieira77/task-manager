2c11af65f3feae1e81f0a508dccf707e53e6b2e6
Alessandro-Vieira77 <alessandrovieiraa77@gmail.com>
2025-09-12 22:58:25 -0300

    feat: add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import { FaRegTrashCan } from "react-icons/fa6";
+import { IoMdAdd } from "react-icons/io";
+
+import Button from "./Button";
+function Header() {
+  return (
+    <div className="flex w-full justify-between px-8 pt-16">
+      <div className="flex flex-col gap-1">
+        <h3 className="text-xs font-semibold text-[#00ADB5]">Minhas Tarefas</h3>
+        <h2 className="text-xl font-semibold text-[#35383E]">Minhas Tarefas</h2>
+      </div>
+
+      <div className="flex items-end gap-2">
+        <Button title="Limpar tarefas" icon={<FaRegTrashCan size={16} />} color="secondary" />
+        <Button title="Criar Tarefa" icon={<IoMdAdd size={16} />} />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
