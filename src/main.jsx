69c07d027ddbd135586030a5662490f29e9e93f9
Alessandro-Vieira77 <alessandrovieiraa77@gmail.com>
2025-09-13 15:30:12 -0300

    chore: add axios and react-query

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import "./index.css";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import PageTasks from "./pages/PageTasks";
+
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
@@ -15,6 +18,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>,
 );
