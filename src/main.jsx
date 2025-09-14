c3fb5cd3dade0ab4aa1f13327cc243f50b92b9e1
Alessandro-Vieira77 <alessandrovieiraa77@gmail.com>
2025-09-14 12:05:56 -0300

    chore: added react-hot-toast and uuid

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Toaster } from "react-hot-toast";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import PageTasks from "./pages/PageTasks";
@@ -19,6 +20,7 @@ const router = createBrowserRouter([
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
+      <Toaster />
       <RouterProvider router={router} />
     </QueryClientProvider>
   </StrictMode>,
