a9b9d4d64528025a213f36a8083e2d941913f821
Alessandro-Vieira77 <alessandrovieiraa77@gmail.com>
2025-09-17 12:19:26 -0300

    refactor: added new hook  taskId

---
+++ src/hooks/use-get-taskId.js
@@ -0,0 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
+import { toast } from "react-hot-toast";
+
+import { queryKey } from "../key/query";
+import api from "../lib/axios";
+
+function useGetTaskId(id) {
+  return useQuery({
+    queryKey: queryKey.getTasks(id),
+    queryFn: async () => {
+      const { data: tasks } = await api.get(`/tasks/${id}`);
+      return tasks;
+    },
+    onError: error => {
+      console.log(error);
+      toast.error("Erro ao buscar tarefas");
+    },
+  });
+}
+
+export default useGetTaskId;
